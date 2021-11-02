21a514f97996e054189f9f2576b41a1d82362246
Romuald-Big-Screen <romuald.torro@gmail.com>
2021-11-02 06:59:48 +0100

    first commit

---
+++ src/main/java/com/SuperHeroquadInterfaceNoData/SuperHeroquadInterfaceNoData/SuperHeroquadInterfaceNoDataApplication.java
@@ -0,0 +1,13 @@
+package com.SuperHeroquadInterfaceNoData.SuperHeroquadInterfaceNoData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperHeroquadInterfaceNoDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperHeroquadInterfaceNoDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SuperHeroquadInterfaceNoData/SuperHeroquadInterfaceNoData/SuperHeroquadInterfaceNoDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SuperHeroquadInterfaceNoData.SuperHeroquadInterfaceNoData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperHeroquadInterfaceNoDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
